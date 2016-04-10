93c42a1f9db83b380d87136c82fd3ff15e76697d
jrue3084 <jrue3084@cox.net>
2016-04-09 23:40:50 -0500

    added settings page

---
+++ app/routes/application.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	currentTransition: null,
+	beforeModel: function(transition){
+		this.baseUrlRedirect(transition);
+		//this.authCheck(transition);
+		//will have other stuff here once its connected to restapi
+	},
+	baseUrlRedirect: function(transition){
+		var t = this;
+		if(transition.targetName === 'index'){
+				t.set('currentTransition', transition);
+				transition.abort();
+				t.transitionTo('home');
+			}
+	},
+	actions: {
+		willTransition: function(transition){
+			this.baseUrlRedirect(transition);
+			//this.authCheck(transition);
+		}
+	}
+});
+
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,95 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  // Materialize UI
+  app.import('vendor/materialize/js/materialize.js');
+  app.import('vendor/materialize/css/materialize.css');
+
+  /*app.import('vendor/materialize/font/material-design-icons/Material-Design-Icons.eot', {
+    destDir: 'font'
+  });
+  app.import('vendor/materialize/font/material-design-icons/Material-Design-Icons.svg', {
+    destDir: 'font'
+  });
+  app.import('vendor/materialize/font/material-design-icons/Material-Design-Icons.ttf', {
+    destDir: 'font'
+  });
+  app.import('vendor/materialize/font/material-design-icons/Material-Design-Icons.woff', {
+    destDir: 'font'
+  });
+  app.import('vendor/materialize/font/material-design-icons/Material-Design-Icons.woff2', {
+    destDir: 'font'
+  });*/
+
+  app.import('vendor/materialize/font/roboto/Roboto-Bold.eot', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Bold.ttf', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Bold.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Bold.woff2', {
+    destDir: 'font/roboto'
+  });
+
+  app.import('vendor/materialize/font/roboto/Roboto-Light.eot', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Light.ttf', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Light.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Light.woff2', {
+    destDir: 'font/roboto'
+  });
+
+  app.import('vendor/materialize/font/roboto/Roboto-Medium.eot', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Medium.ttf', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Medium.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Medium.woff2', {
+    destDir: 'font/roboto'
+  });
+
+  app.import('vendor/materialize/font/roboto/Roboto-Regular.eot', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Regular.ttf', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Regular.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Regular.woff2', {
+    destDir: 'font/roboto'
+  });
+
+  app.import('vendor/materialize/font/roboto/Roboto-Thin.eot', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Thin.ttf', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Thin.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('vendor/materialize/font/roboto/Roboto-Thin.woff2', {
+    destDir: 'font/roboto'
+  });
+
+  // D3 and C3 Charting libraries
+  app.import('vendor/d3-3.5.16/d3.min.js');
+  app.import('vendor/c3-0.4.10/c3.min.js');
+  app.import('vendor/c3-0.4.10/c3.min.css');
+
   return app.toTree();
 };
