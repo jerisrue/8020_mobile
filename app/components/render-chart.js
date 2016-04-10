28da1a82b2c34c3fb8b12cfca3a6d4ebb92e4d8c
jrue3084 <jrue3084@cox.net>
2016-04-09 19:08:07 -0500

    chart and pie config tuning

--- app/components/render-chart.js
+++ app/components/render-chart.js
@@ -21,13 +21,18 @@ export default Ember.Component.extend({
             bindto: '#chart',
             data: {
                 columns: [
+                    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
                     ['healthy', 60, 80, 80, 60, 85, 75],
                     ['unhealthy', 40, 20, 20, 40, 15, 25],
                 ],
                 type: 'bar',
                 groups: [
                     ['healthy', 'unhealthy' ]
-                ]
+                ],
+                colors: {
+                    healthy: '#33ff33',
+                    unhealthy: '#ff0033'
+                },
             },
             grid: {
                 y: {
